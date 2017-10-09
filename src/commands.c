a019473b2ad1728f773d54b232c0650f09040e3a
Kim Sujin <ros008@naver.com>
2017-10-09 13:43:30 +0900

    Implements cd and pwd function

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,46 @@
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
-  if (!validate_cd_argv(argc, argv))
-    return -1;
+        if (!validate_cd_argv(argc, argv))
+                return -1;
+        //chdir(argv[1]);
 
-  // TODO: Fill it!
-  return 0;
+        return 0; 
 }
 
 int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
-    return -1;
+        if (!validate_pwd_argv(argc, argv))
+                return -1;
 
-  // TODO: Fill it!
+        char buffer[256];
+        getcwd(buffer, 256);
+        printf("%s\n", buffer);
 
-  return 0;
+        return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+        if(argc > 2)
+                return 0;
+
+        ////////////not existing file or directory
+        else if(chdir(argv[1])!=0)//not succeed
+                return 0;
+
+
+        return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+        char buffer[256];
+        if(argc != 1)
+                return 0;
+        else if(getcwd(buffer, 256) == NULL)
+                return 0;
+
+        return 1;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,77 @@
 #include "utils.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+        char **ws;
+        char *t;
+        const char *temp;
+        const char *word;
+        int num = 0;
+        int i;
+        int sp = 1;
+        int wlen = 0;
+
+
+        temp = command;
+
+        if(*temp){
+                while(*temp){
+                        if(*temp == ' ' || *temp == '\t' || *temp == '\n'){
+                                sp = 1;
+                        }
+                        else{
+                                if(sp)
+                                        num++;
+                                sp =0;
+                        }
+                        *temp++;
+                }
+
+                ws = (char **)malloc(sizeof(char *)*num);
+                t = (char *)malloc(strlen(command)+1);
+
+                strcpy(t, command);
+
+                i = 0;
+                sp = 1;
+                word = NULL;
+                while(*t){
+                        if(*t == ' ' || *t == '\n' || *t == '\t'){
+                                if(wlen>0){
+                                        ws[i] = (char *)malloc(sizeof(char)*wlen+1);
+                                        strncpy(ws[i], word, wlen);
+                                        ws[i][wlen] = '\0';
+                                        i++;
+                                }
+                                wlen = 0;
+                        }
+
+                        else{
+                                if(wlen == 0){
+                                        word = t;
+
+                                }
+                                wlen++;
+                        }
+                        t++;
+                }
+                if(wlen){
+                         ws[i] = (char *)malloc(sizeof(char)*wlen+1);
+                         strncpy(ws[i], word, wlen);
+                         ws[i][wlen] = '\0';
+                }
+        }
+        if(num == 0){
+                ws[0] = (char *)malloc(sizeof(char)*1);
+                ws[0][0] = 0;
+                num++;
+        }
+        *argc = num;
+        *argv = ws;
 }
